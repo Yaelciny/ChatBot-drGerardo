c3aa9f2f1edef6552526c84a6803a75da79513c4
Yaelciny <mag07yael@gmail.com>
2026-05-20 13:28:14 -0600

    Implementacion de ia para respuestas de consultas, y seccion terminada

--- src/flows/consultasFLow.js
+++ src/flows/consultasFLow.js
@@ -1,9 +1,35 @@
 import path from 'path';
 import fs from 'fs';
 import { addKeyword, EVENTS } from '@builderbot/bot';
+import chat from '../chatGpt/chatGpt.js'
 
 const saludoPath = path.join(process.cwd(), 'mensajes', 'consultas', 'saludo.txt')
 const saludo = fs.readFileSync(saludoPath, 'utf-8')
 
+const promptPath = path.join(process.cwd(), 'mensajes', 'consultas', 'prompt.txt')
+const prompt = fs.readFileSync(promptPath, 'utf-8')
+
+const salidaPath = path.join(process.cwd(), 'mensajes', 'consultas', 'salida.txt')
+const salida = fs.readFileSync(salidaPath, 'utf-8')
+
 export const flowConsulta = addKeyword(EVENTS.ACTION)
-    .addAnswer(saludo)
+    .addAnswer(
+        saludo,
+        { capture: true },
+        async (ctx, ctxFn) => {
+            const consultaUsuario = ctx.body.toLowerCase();
+            // Conidicion para salir del flujo
+            if (consultaUsuario.includes('menu') || consultaUsuario.includes('menú') || consultaUsuario === '0') {
+                return;
+            }
+            // funcion de chatgpt
+            const answer = await chat(prompt, ctx.body);
+
+            await ctxFn.flowDynamic(answer);
+
+            // Metemos el texto de salida en el fallback
+            // para que no regrese al saludo
+            return ctxFn.fallBack(salida);
+        }
+
+    )
--- src/flows/nat.js
+++ src/flows/nat.js
@@ -1,11 +1,13 @@
 import { welcomeFlow } from './welcomeFlow.js';
 import { menuFlow } from './menuFlow.js';
 import { flowInfoGeneral } from './infoGeneralFlow.js';
+import { flowConsulta } from './consultasFLow.js';
 
 
 // Exportamos un arreglo con todos los flujos listos para ser consumidos
 export default [
     welcomeFlow,
     flowInfoGeneral,
+    flowConsulta,
     menuFlow
 ];
